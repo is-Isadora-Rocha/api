8447618157129597e581611dbbcbfc497ab035e0
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-07 09:50:29 -0300

    sobre module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-@Module({
+@Module({ //DECORATOR Module-> começam com @
   imports: [],
   controllers: [AppController],
   providers: [AppService],
+  exports: [AppService] //acesso ao meu AppService quem importar o AppModule
 })
 export class AppModule {}
