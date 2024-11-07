ad08bd48c41493d226727648aae24a3ef61ca5b9
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-07 10:46:19 -0300

    add exports ao module user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,7 +3,8 @@ import { Module } from "@nestjs/common";
 @Module({
     imports: [],
     controllers: [],
-    providers: [] //service injetado
+    providers: [], //service injetado
+    exports: []
 })
 export class UserModule {
 
