b7c7128ce309c05b1066d9c01d06ad40dd68f57e
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-07 10:44:59 -0300

    sobre @infectable

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable() //pode injetar essa classe em outras classes
 export class AppService {
   getHello(): string {
     return 'Hello World!';
