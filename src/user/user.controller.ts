520f8fd9cc6c2220c7fd5160cf53b3735424093e
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-07 21:28:02 -0300

    rotas get all e get id por parametro

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,20 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 
 @Controller('users')
 export class UserController {
 
     @Post()
-    async create() {
-        return {};
+    async create(@Body() body) {
+        return {body};
+    }
+
+    @Get()
+    async read() {
+        return { users: [] }
+    }
+
+    @Get(':id')
+    async readOne(@Param() param) {
+        return { user: [], param }
     }
 }
