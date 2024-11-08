ae40cda4be0b5a24272acd2d4975c667645f1ec1
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-07 21:27:21 -0300

    importe de UserModule para o AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({ //DECORATOR Module-> começam com @
-  imports: [],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [AppService] //acesso ao meu AppService quem importar o AppModule
